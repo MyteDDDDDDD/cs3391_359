90330a929fd8b040a8ddfad810053be7f489410d
MyteDDDDDDD <elvisyuan6988@gmail.com>
2017-01-18 21:40:47 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a; // records the input test cases
+	string ins=00000000; // records instruction
+	cin >> a;
+
+	while (!cin.eof()) {
+		cin >> a;
+		for (int i = 0; i < 3; i++) {//read first 3 chars of a
+			
+		}
+	}
+	
+	return 0;
+}
